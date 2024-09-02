3a8989b9067006c2eef6507399173fe36bbe5540
agvxx <157505707+agvxx@users.noreply.github.com>
2024-09-01 22:39:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,4 +35,19 @@ document.getElementById('meuFormulario').addEventListener('submit', function(eve
     console.log('Mensagem:', message);
   }
   
+  
 });
+
+let currentIndex = 0;
+    const images = document.querySelectorAll('.intro-img img');
+    const totalImages = images.length;
+
+    function showNextImage() {
+        images[currentIndex].classList.remove('active'); 
+        currentIndex = (currentIndex + 1) % totalImages; 
+        images[currentIndex].classList.add('active');
+    }
+
+    setInterval(showNextImage, 3000);
+
+    images[currentIndex].classList.add('active');
